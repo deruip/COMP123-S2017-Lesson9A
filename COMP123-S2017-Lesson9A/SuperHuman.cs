d02e129dfd571f7132e49fa2df4cdfce4e844de3
deruip <aronly1997@hotmail.com>
2017-07-11 15:41:43 -0400

    Added SuperHuman sub class

---
+++ COMP123-S2017-Lesson9A/SuperHuman.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+ * Name: Aron Ly
+ * Date: July 11, 2017
+ * Description: This is the SuperHuman sub class
+ * Version: 0.1 - Class created
+*/
+namespace COMP123_S2017_Lesson9A
+{
+    /// <summary>
+    /// This is the SuperHuman sub class.
+    /// It inherits from the Human SuperClass
+    /// </summary>
+    public class SuperHuman : Human
+    {
+        // Private Fields
+
+
+        // Public Properties
+
+        // Constructors
+        public SuperHuman(string name)
+            :base(name)
+        {
+
+        }
+        // Public Methods
+    }
+}
