7e5e029bc651efae6cdb4a1c9f7301066dc4f220
deruip <aronly1997@hotmail.com>
2017-07-11 15:46:30 -0400

    Added Power Class

---
+++ COMP123-S2017-Lesson9A/Power.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+ * Name: Aron Ly
+ * Date: July 11, 2017
+ * Description: This is the new Power data type
+ * Version: 0.1 - Power Class created
+*/
+namespace COMP123_S2017_Lesson9A
+{
+    public class Power
+    {
+        /// <summary>
+        /// The Power class contains the data and structure of each Super Power.
+        /// </summary>
+        public string Name { get; set; }
+        public int Rank { get; set; }
+    }
+}
